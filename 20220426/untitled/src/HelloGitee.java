858992af7c6139d0ea768b0975c7342bd34a894f
加油昕玉 <10162859+come-on-xinyu@user.noreply.gitee.com>
2022-04-26 18:35:28 +0800

    提交第一次Java代码

---
+++ 20220426/untitled/src/HelloGitee.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: xinyu
+ * Date: 2022-04-26
+ * Time: 18:32
+ */
+public class HelloGitee {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
