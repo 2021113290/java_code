0635165c66661b8e49afbf615b13e4adb8d47e44
加油昕玉 <10162859+come-on-xinyu@user.noreply.gitee.com>
2022-05-04 23:01:40 +0800

    计算最后一个单词长度

--- practice2/src/Test.java
+++ practice2/src/Test.java
@@ -42,7 +42,7 @@ public class Test {
         System.out.println(Arrays.toString(arr));
     }
 
-        public static void main(String[] args) {
+        public static void main3(String[] args) {
             Scanner scanner = new Scanner(System.in);
             int a = scanner.nextInt();
             int b = scanner.nextInt();
@@ -53,5 +53,25 @@ public class Test {
 
 
         }
+    public static void main4(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String[] s = str.split(" "); //正则表达式实用性更强( str.split("\\s+"))
+        int length = s[s.length - 1].length();
+        System.out.println(length);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int length = str.length();
+        int count = 0;
+        for (int i = length - 1; i >= 0; i--) {
+            if (str.charAt(i)==' ') { // 或者 if (str.substring(i, i + 1).equals(" ")) {
+                break;
+            }
+            count++;
+        }
+        System.out.println(count);
+    }
 
 }
