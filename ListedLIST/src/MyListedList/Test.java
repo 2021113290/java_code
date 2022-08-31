97ff3b09eed9b9cbf2e42dcc198734b77b5e150e
加油昕玉 <10162859+come-on-xinyu@user.noreply.gitee.com>
2022-09-01 00:58:51 +0800

    ***反转链表*** ***给定一个带有头节点head的非空单链表，返回链表的中间节点。如果有两个中间节点，则返回第二个中间节点 ***输出倒数第k个节点 ***合并两个链表，使其为升序

--- ListedLIST/src/MyListedList/Test.java
+++ ListedLIST/src/MyListedList/Test.java
@@ -8,6 +8,31 @@ package MyListedList;
  * Time: 22:13
  */
 public class Test {
+    //合并两个链表，使其为升序
+    public TestSingleList.ListNode mergeTwoLists(TestSingleList.ListNode head1,TestSingleList.ListNode head2){
+        TestSingleList.ListNode newHead=new TestSingleList.ListNode(-1);//傀儡节点，虚拟节点，其实就是dummy
+        TestSingleList.ListNode tmp=newHead;
+        while (head1!=null&&head2!=null){
+            if (head1.val<head2.val){
+                tmp.next=head1;
+                head1=head1.next;
+                tmp=tmp.next;
+            }else {
+                tmp.next=head2;
+                head2=head2.next;
+                tmp=tmp.next;
+            }
+        }
+        //跳出循环，说明有两种情况：1.head2==null   2.head1==null
+        if (head1!=null){
+            tmp.next=head1;
+        }
+        if (head2!=null){
+            tmp.next=head2;
+        }
+        return newHead.next;
+    }
+
     public static void main(String[] args) {
         TestSingleList test=new TestSingleList();
         test.addFirst(1);
--- ListedLIST/src/MyListedList/TestSingleList.java
+++ ListedLIST/src/MyListedList/TestSingleList.java
@@ -8,17 +8,17 @@ package MyListedList;
  * Time: 21:41
  */
 public class TestSingleList {
-    static class Node{
+    static class ListNode {
         public int val;
-        public Node next;
-        public Node(int val){
+        public ListNode next;
+        public ListNode(int val){
             this.val=val;
         }
     }
-    public Node head;//表示存储当前链表的头节点的引用
+    public ListNode head;//表示存储当前链表的头节点的引用
     //打印顺序表
     public void display(){
-        Node cur=this.head;
+        ListNode cur=this.head;
         while (cur!=null){
             System.out.println(cur.val+" ");
             cur=cur.next;
@@ -26,7 +26,7 @@ public class TestSingleList {
     }
     //查找表里是否含有key元素
     public boolean contains(int key) {
-        Node cur = head;
+        ListNode cur = head;
         while (cur != null) {
             if (cur.val == key) {
                 return true;
@@ -37,7 +37,7 @@ public class TestSingleList {
     }
     //得到单链表长度
     public int size(){
-        Node cur=head;
+        ListNode cur=head;
         int count=0;
         while (cur!=null){
             count++;
@@ -47,17 +47,17 @@ public class TestSingleList {
     }
     //头插法
     public void addFirst(int data){
-        Node node=new Node(data);
+        ListNode node=new ListNode(data);
         node.next=head;
         head=node;
     }
     //尾插法o(n)
     public void addLast(int data){
-        Node node=new Node(data);
+        ListNode node=new ListNode(data);
         if (head==null){
             head=node;
         }else{
-            Node cur=head;
+            ListNode cur=head;
             while (cur.next!=null){
                 cur=cur.next;
             }
@@ -80,13 +80,13 @@ public class TestSingleList {
             return;
         }
         //说明，不是头插尾插且index合法
-        Node node=new Node(data);
-        Node cur=findIndexSubOfOne(index);
+        ListNode node=new ListNode(data);
+        ListNode cur=findIndexSubOfOne(index);
         node.next=cur.next;
         cur.next=node;
     }
-    private  Node findIndexSubOfOne(int index){
-        Node cur=head;
+    private ListNode findIndexSubOfOne(int index){
+        ListNode cur=head;
         while (index-1!=0){
             cur=cur.next;
             index--;
@@ -99,13 +99,13 @@ public class TestSingleList {
             head=head.next;
             return;
         }
-        Node cur=searchPreOfKey(key);
+        ListNode cur=searchPreOfKey(key);
         if (cur==null)return;
         cur.next=cur.next.next;
     }
     //找到关键字为key的前驱
-    private Node searchPreOfKey(int key){
-        Node cur=head;
+    private ListNode searchPreOfKey(int key){
+        ListNode cur=head;
         while(cur.next!=null){
             if (cur.next.val==key){
                 return cur;
@@ -117,8 +117,8 @@ public class TestSingleList {
     //删除所有值为key的节点,******面试题*******
     public void removeAllKey(int key){
         if (head==null)return;
-        Node prev=head;
-        Node cur=head.next;
+        ListNode prev=head;
+        ListNode cur=head.next;
         while (cur!=null){
             if (cur.val==key) {
                 prev.next = cur.next;
@@ -135,13 +135,59 @@ public class TestSingleList {
     }
     //清空链表
     public void clear(){
-        Node cur=head;
+        ListNode cur=head;
         while (cur!=null){
-            Node curNext=cur.next;
+            ListNode curNext=cur.next;
             cur.next=null;
             cur=curNext;
         }
         head=null;
         //或者直接this.head=null;
+    }
+    //面试题，***反转链表***
+    public ListNode reverseList(){
+        if(head==null)return null;//说明链表是空的
+        if (head.next==null)return head;//说明只有一个节点
+        ListNode cur=head.next;//当前需要翻转的节点
+        head.next=null;//head其实就是当前需要翻转的节点的前驱
+        while (cur!=null){
+            ListNode curNext=cur.next;
+            cur.next=head;
+            head=cur;
+            cur=curNext;
+        }
+        return head;
+        //这个做法的思想就是，先把头节点的指针域置为空就没有链接了，然后把cur头插
+        //leetcode上有另外一种解法，就是再设置一个前置节点dummy
+    }
+    //**面试题**：给定一个带有头节点head的非空单链表，返回链表的中间节点。如果有两个中间节点，则返回第二个中间节点
+    //思路：遍历一次，更高效。快慢指针，路程一样，一个走一步，一个走两步。fast走到末尾，slow走到中间
+    public ListNode middleNode(ListNode head){
+        if (head==null)return null;//注意空链表
+        ListNode fast=head;
+        ListNode slow=head;
+        while (fast!=null&&fast.next==null){//先后顺序判断不能颠倒且一定用&&
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+        return slow;
+    }
+    //面试题****输出倒数第k个节点******、
+    //面试要求：k合法的情况下，遍历一次，快指针先走k-1步，然后一起走，当fast.next==null时，slow就是倒数第k个
+    public ListNode FindKthToTail(int k){
+        if (k<=0||k>size()){
+            return null;
+        }
+        ListNode fast=head;
+        ListNode slow=head;
+        while (k-1!=0){
+            fast=fast.next;
+            k--;
+        }
+        while (fast.next!=null){
+            fast=fast.next;
+            slow=slow.next;
+        }
+        return slow;
     }
 }
