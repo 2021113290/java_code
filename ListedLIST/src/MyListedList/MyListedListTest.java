b02f6511cef1eedfddfdeb5337b15d3644e5ae25
加油昕玉 <10162859+come-on-xinyu@user.noreply.gitee.com>
2023-02-26 16:47:50 +0800

    尝试第一次同步代码

---
+++ ListedLIST/src/MyListedList/MyListedListTest.java
@@ -0,0 +1,47 @@
+package MyListedList;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: xinyu
+ * Date: 2023-02-24
+ * Time: 15:02
+ */
+public class MyListedListTest {
+
+    @Test
+    public void display() {
+    }
+
+    @Test
+    public void addFirst() {
+    }
+
+    @Test
+    public void addLast() {
+    }
+
+    @Test
+    public void addIndex() {
+    }
+
+    @Test
+    public void remove() {
+    }
+
+    @Test
+    public void contains() {
+    }
+
+    @Test
+    public void size() {
+    }
+
+    @Test
+    public void clear() {
+    }
+}
