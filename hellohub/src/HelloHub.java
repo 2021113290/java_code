550877f8fb925bda7f8d30d6ae957ddb4010ae77
加油昕玉 <10162859+come-on-xinyu@user.noreply.gitee.com>
2023-02-26 16:35:22 +0800

    尝试第一次同步代码

---
+++ hellohub/src/HelloHub.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: xinyu
+ * Date: 2023-02-26
+ * Time: 16:22
+ */
+public class HelloHub {
+    public static void main(String[] args){
+        System.out.println("Hellohub");
+    }
+}
