163c5e5c628127f66924e8a5c9c5d42592c07a74
加油昕玉 <10162859+come-on-xinyu@user.noreply.gitee.com>
2023-02-28 12:58:04 +0800

    lab1

---
+++ QAQ/src/QAQ.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: xinyu
+ * Date: 2023-02-26
+ * Time: 17:31
+ */
+public class QAQ {
+    public static void main(String[] args){
+        System.out.println("wuwuwu");
+    }
+}
